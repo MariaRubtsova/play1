3d0f17f9814ffe4b2855f18950e0e14fda54d924
MariaRubtsova <124884461+MariaRubtsova@users.noreply.github.com>
2024-05-20 04:15:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import csv
 
 pygame.init()
 
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
@@ -14,32 +13,56 @@ pygame.display.set_caption('Kings and Pigs')
 clock = pygame.time.Clock()
 FPS = 60
 
-RED = (255, 0, 0)
+BLACK = (0, 0, 0)
 GRAVITY = 0.75
 ROWS = 16
 COLS = 150
 TILE_SIZE = SCREEN_HEIGHT // ROWS
-TILE_TYPES = 24
+TILE_TYPES = 26
+screen_scroll = 0
+bg_scroll = 0
 
 moving_left = False
 moving_right = False
 moving_down = False
 
-#хранить плитки в списке
+
+# хранить плитки в списке
 img_list = []
-for x  in range(TILE_TYPES):
+for x in range(TILE_TYPES):
     img = pygame.image.load(f'img/tile/{x}.png')
     img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
     img_list.append(img)
 
+diamonds_img = pygame.image.load('img/diamonds/25.png').convert_alpha()
+diamonds_img = pygame.transform.scale(diamonds_img, (20, 17))
+background = pygame.image.load('img/background/fon.png')
+background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+heart_img = pygame.image.load('img/health_bar/2.png').convert_alpha()
+health_bar_img = pygame.image.load('img/health_bar/1.png').convert_alpha()
+health_bar_x = 10
+health_bar_y = 65
+bar_img = pygame.image.load('img/health_bar/3.png').convert_alpha()
+bar_x = 10
+bar_y = 5
+
+
+font = pygame.font.SysFont('ARCADEPI', 30)
+
+def draw_text(text, font, text_col, x, y):
+	img = font.render(text, True, text_col)
+	screen.blit(img, (x, y))
+
+
 class Hero(pygame.sprite.Sprite):
-    def __init__(self, char_type, x, y, speed, anim_speed):
+    def __init__(self, char_type, health, x, y, speed, anim_speed):
         pygame.sprite.Sprite.__init__(self)
         self.alive = True
         self.char_type = char_type
         self.speed = speed
         self.anim_speed = anim_speed
-        self.health = 3
+        self.diamonds = 0
+        self.health = health
         self.direction = 1
         self.vel_y = 0
         self.jump = False
@@ -58,7 +81,7 @@ class Hero(pygame.sprite.Sprite):
             temp_list = []
             num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
             for i in range(num_of_frames):
-                img = pygame.image.load(f'img/{self.char_type}/{animation}/{i+1}.png')
+                img = pygame.image.load(f'img/{self.char_type}/{animation}/{i + 1}.png')
                 img = pygame.transform.scale(img, (int(img.get_width()), int(img.get_height())))
                 temp_list.append(img)
             self.animation_list[animation] = temp_list
@@ -66,8 +89,11 @@ class Hero(pygame.sprite.Sprite):
         self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
 
     def move(self, moving_left, moving_right):
+        screen_scroll = 0
         dx = 0
         dy = 0
         if moving_left:
@@ -86,12 +112,58 @@ class Hero(pygame.sprite.Sprite):
             self.jump = False
             self.in_air = True
 
+        def update(self):
+            self.vel_y += GRAVITY
+            dx = self.direction * self.speed
+            dy = self.vel_y
+
+            # check for collision with level
+            for tile in world.obstacle_list:
+                # check collision with walls
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    self.direction *= -1
+                    dx = self.direction * self.speed
+                # check for collision in the y direction
+                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    self.speed = 0
+                    # check if below the ground, i.e. thrown up
+                    if self.vel_y < 0:
+                        self.vel_y = 0
+                        dy = tile[1].bottom - self.rect.top
+                    # check if above the ground, i.e. falling
+                    elif self.vel_y >= 0:
+                        self.vel_y = 0
+                        dy = tile[1].top - self.rect.bottom
+
         self.vel_y += GRAVITY
         dy += self.vel_y
 
-        if self.rect.bottom + dy > 300:
-            dy = 300 -self.rect.bottom
-            self.in_air = False
+        def ai(self):
+            if self.alive and player.alive:
+                if self.direction == 1:
+                    ai_moving_right = True
+                else:
+                    ai_moving_left = False
+                ai_moving_left = not ai_moving_right
+                self.move(ai_moving_left, ai_moving_right)
+
+        # проверка препятсвий
+        for tile in world.obstacle_list:
+            # проверка препятсвий в направлении х
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                dx = 0
+            # проверка препятсвий в направлении y
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                dx = 0
+                # проверка, если ниже земли, то прыгает
+                if self.vel_y < 0:
+                    self.vel_y = 0
+                    dy = tile[1].bottom - self.rect.top
+                # проверка, если находится над землей, то падает
+                elif self.vel_y >= 0:
+                    self.vel_y = 0
+                    self.in_air = False
+                    dy = tile[1].top - self.rect.bottom
 
         self.rect.x += dx
         self.rect.y += dy
@@ -103,6 +175,11 @@ class Hero(pygame.sprite.Sprite):
         self.attack_rect = pygame.Rect((self.rect.left + left_shift, self.rect.top), (50, self.rect.height))
 
     def update_animation(self):
+        if not isinstance(self.action, str):
+            raise TypeError(f"self.action должно быть строкой, но найдено {type(self.action).__name__}")
+        if not isinstance(self.frame_index, int):
+            raise TypeError(f"self.frame_index должно быть целым числом, но найдено {type(self.frame_index).__name__}")
+
         self.image = self.animation_list[self.action][self.frame_index]
         if pygame.time.get_ticks() - self.update_time > self.anim_speed:
             self.frame_index += 1
@@ -129,12 +206,13 @@ class Hero(pygame.sprite.Sprite):
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
+
 class World():
     def __init__(self):
         self.obstacle_list = []
 
     def process_data(self, data):
-        #перебирать каждое значение в level data file
+        # перебирать каждое значение в level data file
         for y, row in enumerate(data):
             for x, tile in enumerate(row):
                 if tile >= 0:
@@ -143,21 +221,18 @@ class World():
                     img_rect.x = x * TILE_SIZE
                     img_rect.y = y * TILE_SIZE
                     tile_data = (img, img_rect)
-                    if tile >=0 and tile <= 21:
-                        self.obstacle_list.append(tile_data) #препятствия
-                    elif tile > 22:
-                        decoration = Decoration(img, x * TILE_SIZE, y * TILE_SIZE)
-                        decoration_group.add(decoration) #украшение
-
-                    elif tile == 22:#create player
-                        player = Hero('player', x * TILE_SIZE, y * TILE_SIZE, 5, 100)
-                    """
+                    if tile >= 0 and tile <= 21:
+                        self.obstacle_list.append(tile_data)  # препятствия
+                    elif tile == 22:  # create player
+                        player = Hero('player', 3, x * TILE_SIZE, y * TILE_SIZE, 5, 100)
                     elif tile == 23:#create pig
-                        player = Hero('pig', x * TILE_SIZE, y * TILE_SIZE,...)
-                    elif tile == 23:#create diamond
-                        diamond = Diamond('Diamond', x * TILE_SIZE, y * TILE_SIZE,...)
-                    
-                    """
+                        pig = Hero('pig',1,  x * TILE_SIZE, y * TILE_SIZE,5, 100)
+                    elif tile == 24: #create diamonds
+                        diamonds = Diamonds('Diamonds', x * TILE_SIZE, y * TILE_SIZE)
+                        Diamonds_group.add(diamonds)
+                    elif tile == 25:
+                        decoration = Decoration(img, x * TILE_SIZE, y * TILE_SIZE)
+                        decoration_group.add(decoration)  # украшение
 
         return player
 
@@ -165,27 +240,48 @@ class World():
         if self.obstacle_list:
             for tile in self.obstacle_list:
                 screen.blit(tile[0], tile[1])
-        else:
-            print("No tiles to draw")
+
 
 class Decoration(pygame.sprite.Sprite):
-    def __init__(self, item_tipe, x, y):
+	def __init__(self, img, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = img
+		self.rect = self.image.get_rect()
+		self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()))
+
+class Diamonds(pygame.sprite.Sprite):
+    def __init__(self, item_type, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = img
+        self.item_type = item_type
+        if self.item_type == 'Diamonds':
+            self.image = diamonds_img
         self.rect = self.image.get_rect()
-        self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()))
+        self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()*1.5))
 
-pig = Hero('player', 400, 200, 5, 100)
 
+    def update(self):
+        #проверка, взял ли игрок бриллиант
+        if pygame.sprite.collide_rect(self, player):
+            if self.item_type == 'Diamonds':
+                player.diamonds += 1
+            self.kill()
+
+
+
+pig = Hero('pig', 1, 400, 580, 5, 100)
+
+Diamonds_group = pygame.sprite.Group()
 decoration_group = pygame.sprite.Group()
 
-#создать пустой список плиток
+
+
+# создать пустой список плиток
 world_data = []
 for row in range(ROWS):
     r = [-1] * COLS
     world_data.append(r)
 
-#загружаем данные и создаем мир
+# загружаем данные и создаем мир
 with open(f'Level0_data.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for x, row in enumerate(reader):
@@ -197,7 +293,6 @@ player = world.process_data(world_data)
 
 run = True
 while run:
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -241,19 +336,37 @@ while run:
 
 
 
-        screen.fill((144, 201, 120))
+        screen.blit(background, (0, 0))
+
         world.draw()
-        pygame.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
         player.update_animation()
-        decoration_group.update()
+
+        player.update()
         player.draw()
+
+        pig.update()
         pig.draw()
+
+        Diamonds_group.update()
+        decoration_group.update()
         decoration_group.draw(screen)
+        Diamonds_group.draw(screen)
         player.move(moving_left, moving_right)
+
+        screen.blit(health_bar_img, (health_bar_x, health_bar_y))
+        draw_text(f'{player.diamonds}', font, BLACK, 80, 90)
+
+        screen.blit(bar_img, (bar_x, bar_y))
+        for x in range(player.health):
+            screen.blit(heart_img, (46 + (x * 22), 32))
+
         pygame.display.update()
         clock.tick(FPS)
 
+
 pygame.quit()
+
+
 
 
 
